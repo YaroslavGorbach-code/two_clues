754a2581eddb4f61301dd80d058cbfb1fe270bc0
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2022-05-30 21:35:32 +0300

    Minor bug fix

--- app/src/main/java/andersen/two_clues/data/puzzle/provider/TaskCluesProvider.kt
+++ app/src/main/java/andersen/two_clues/data/puzzle/provider/TaskCluesProvider.kt
@@ -2,7 +2,6 @@ package andersen.two_clues.data.puzzle.provider
 
 import andersen.two_clues.data.common.model.PuzzleName
 import android.content.Context
-import android.util.Log
 
 class TaskCluesProvider(
     private val context: Context,
@@ -17,8 +16,6 @@ class TaskCluesProvider(
     fun provider(name: PuzzleName): List<Pair<String, String>> {
         return context.resources.getString(taskIdProvider.provide(name))
             .split(TASKS_SPLIT_SYMBOL)
-            .map { it.split(TASK_SPLIT_SYMBOL) }.map {
-                Log.i("dssd", it.toString())
-                it[0] to it[1] }
+            .map { it.split(TASK_SPLIT_SYMBOL) }.map { it[0] to it[1] }
     }
 }
--- app/src/main/java/andersen/two_clues/data/puzzles/factory/PuzzleAvailabilityProvider.kt
+++ app/src/main/java/andersen/two_clues/data/puzzles/factory/PuzzleAvailabilityProvider.kt
@@ -2,12 +2,13 @@ package andersen.two_clues.data.puzzles.factory
 
 import andersen.two_clues.data.common.model.PuzzleName
 import andersen.two_clues.data.puzzles.PuzzleDataStore
+import kotlinx.coroutines.flow.first
 
 class PuzzleAvailabilityProvider(private val puzzleDataStore: PuzzleDataStore) {
     suspend fun isAvailable(puzzleName: PuzzleName): Boolean {
         return when (puzzleName) {
-            else -> true
-//            else -> puzzleDataStore.isAvailable(puzzleName).first()
+            PuzzleName.LETTER_A -> true
+            else -> puzzleDataStore.isAvailable(puzzleName).first()
         }
     }
 }
